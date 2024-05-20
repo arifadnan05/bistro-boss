dfbea50c25527b6fb2f528596ca910cb8bf1521e
arifadnan05 <arifahamedadnan@gmail.com>
2024-05-21 00:48:56 +0600

    register components all done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,6 +4,7 @@ import loginBg from '../../assets/others/authentication.png'
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 import { useContext, useEffect, useRef, useState } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider';
+import { Helmet } from 'react-helmet-async';
 
 const Login = () => {
     const { signInUser } = useContext(AuthContext)
@@ -21,13 +22,13 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
         signInUser(email, password)
-        .then(result => {
-            const user = result.user;
-            console.log(user)
-        })
-        .catch(error => {
-            console.error(error)
-        })
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => {
+                console.error(error)
+            })
     }
 
 
@@ -43,42 +44,47 @@ const Login = () => {
     }
 
     return (
-        <div className={`hero min-h-screen bg-[url('${loginBg}')] h-64 w-full bg-cover bg-center`}>
-            <div className="hero-content flex space-x-[100px] justify-center">
-                <div className="w-[40%] text-center lg:text-left">
-                    <img src={loginImg} />
-                </div>
-                <div className="card shrink-0 border w-[59%] max-w-sm shadow-2xl bg-base-100">
-                    <form onSubmit={handleLogin} className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input name='email' type="email" placeholder="email" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input name='password' type="password" placeholder="password" className="input input-bordered" required />
-                        </div>
+        <>
+        <Helmet>
+            <title>Bistro Boss | Login</title>
+        </Helmet>
+            <div className={`hero min-h-screen bg-[url('${loginBg}')] h-64 w-full bg-cover bg-center`}>
+                <div className="hero-content flex space-x-[100px] justify-center">
+                    <div className="w-[40%] text-center lg:text-left">
+                        <img src={loginImg} />
+                    </div>
+                    <div className="card shrink-0 border w-[59%] max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleLogin} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input name='email' type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input name='password' type="password" placeholder="password" className="input input-bordered" required />
+                            </div>
 
-                        <div className="form-control">
-                            <label className="label">
-                                <LoadCanvasTemplate />
+                            <div className="form-control">
+                                <label className="label">
+                                    <LoadCanvasTemplate />
 
-                            </label>
+                                </label>
 
-                            <input ref={captchaRef} name='captcha' type="text" placeholder="enter captcha" className="input input-bordered" required />
-                        </div>
-                        <button onClick={handleValidateCaptcha} className="my-4 btn btn-outline btn-xs btn-success">Valid</button>
-                        <div className="form-control mt-6">
-                            <button disabled={disabled} className="btn btn-primary">Login</button>
-                        </div>
-                    </form>
+                                <input ref={captchaRef} name='captcha' type="text" placeholder="enter captcha" className="input input-bordered" required />
+                            </div>
+                            <button onClick={handleValidateCaptcha} className="my-4 btn btn-outline btn-xs btn-success">Valid</button>
+                            <div className="form-control mt-6">
+                                <button disabled={disabled} className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -3,6 +3,7 @@ import loginBg from '../../assets/others/authentication.png'
 import { useContext } from 'react'
 import { AuthContext } from '../../Provider/AuthProvider'
 import { useForm } from 'react-hook-form'
+import { Helmet } from 'react-helmet-async'
 const Register = () => {
 
     const { createUser } = useContext(AuthContext)
@@ -27,44 +28,54 @@ const Register = () => {
     }
 
 
-return (
-    <div className={`hero min-h-screen bg-[url('${loginBg}')] h-64 w-full bg-cover bg-center`}>
-        <div className="hero-content flex space-x-[100px] justify-center">
-            <div className="w-[40%] text-center lg:text-left">
-                <img src={loginImg} />
-            </div>
-            <div className="card shrink-0 border w-[59%] max-w-sm shadow-2xl bg-base-100">
-                <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input name='name' type="text" placeholder="Enter your name" className="input input-bordered" required {...register("name", { required: true })} />
-                        {errors.name && <span>This field is required</span>}
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input name='email' type="email" placeholder="Enter your email" className="input input-bordered" required {...register("email", { required: true })} />
-                        {errors.email && <span>This field is required</span>}
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input name='password' type="password" placeholder="Enter your password" className="input input-bordered" required {...register("password", { required: true })} />
-                        {errors.password && <span>This field is required</span>}
+    return (
+        <>
+        <Helmet>
+            <title>Bistro Boss | Register</title>
+        </Helmet>
+            <div className={`hero min-h-screen bg-[url('${loginBg}')] h-64 w-full bg-cover bg-center`}>
+                <div className="hero-content flex space-x-[100px] justify-center">
+
+                    <div className="card shrink-0 border w-[59%] max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input name='name' type="text" placeholder="Enter your name" className="input input-bordered" required {...register("name", { required: true })} />
+                                {errors.name && <span>This field is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input name='email' type="email" placeholder="Enter your email" className="input input-bordered" required {...register("email", { required: true })} />
+                                {errors.email && <span>This field is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input name='password' type="password" placeholder="Enter your password" className="input input-bordered" required {...register("password", { required: true })} />
+                                {errors.password && <span>This field is required</span>}
+                            </div>
+
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </form>
                     </div>
 
-                    <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
+
+
+                    <div className="w-[40%] text-center lg:text-left">
+                        <img src={loginImg} />
                     </div>
-                </form>
+
+                </div>
             </div>
-        </div>
-    </div>
-)
+        </>
+    )
 }
 
 export default Register
